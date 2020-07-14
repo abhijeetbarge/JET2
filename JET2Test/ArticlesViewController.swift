db88b6fda3645262f4a44ba01ece069cb1de070c
Abhijeet Barge <abhijeet.barge@vuclip.com>
2020-07-14 16:58:18 +0530

    Added project

---
+++ JET2Test/ArticlesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ArticlesViewController.swift
+//  JET2Test
+//
+//  Created by Abhijeet Barge on 14/07/20.
+//  Copyright © 2020 Abhijeet Barge. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JET2Test/UsersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UsersViewController.swift
+//  JET2Test
+//
+//  Created by Abhijeet Barge on 14/07/20.
+//  Copyright © 2020 Abhijeet Barge. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
